113ee2d080c5c19750ad822170ae7dc3ba3e0853
pio1755 <44492933+pio1755@users.noreply.github.com>
2021-12-06 03:01:09 +0100

    Add files via upload

---
+++ lab5/zad2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zad2 : MonoBehaviour
+{
+    public float doorSpeed = 3f;
+    public float distance = 1f;
+    private float startPosition;
+    private float endPosition;
+    private bool open = false;
+    private bool close = false;
+
+    void Start()
+    {
+        startPosition = transform.position.x;
+        endPosition = transform.position.x - distance;
+    }
+
+    void FixedUpdate()
+    {
+        if (open && transform.position.x > endPosition)
+        {
+            transform.Translate(doorSpeed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        else if (close && transform.position.x < startPosition)
+        {
+            transform.Translate(-doorSpeed * Time.deltaTime, 0.0f, 0.0f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player podszedl do drzwi.");
+            open = true;
+            close = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player odszedl od drzwi.");
+            open = false;
+            close = true;
+        }
+    }
+}
